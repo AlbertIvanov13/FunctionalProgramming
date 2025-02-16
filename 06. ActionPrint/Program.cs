b04659e5919b6e79321728dde65c7857b07f7568
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 16:44:10 +0200

    100/100

---
+++ 06. ActionPrint/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06._ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> action = null;
+
+            foreach (var name in names)
+            {
+                action = name => Console.WriteLine(name);
+                action(name);
+            }
+        }
+    }
+}
