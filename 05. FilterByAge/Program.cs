3e1596ea9aa8a36ed855fbe613c131ce9d84f4c5
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 16:12:25 +0200

    100/100

---
+++ 05. FilterByAge/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _05._FilterByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split(", ").ToArray();
+                string personName = personData[0];
+                int personAge = int.Parse(personData[1]);
+
+                Person person = new Person();
+                person.Name = personName;
+                person.Age = personAge;
+                persons.Add(person);
+            }
+
+            string type = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            Func<Person, bool> filter  = GetFilter(type, age);
+            persons = persons.Where(filter).ToList();
+
+            Func<Person, string> formatter = GetFormatter(format);
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(formatter(person));
+            }
+        }
+
+        public static Func<Person, bool> GetFilter(string type, int age)
+        {
+            if (type == "older")
+            {
+                return p => p.Age >= age;
+            }
+            else
+            {
+                return p => p.Age < age;
+            }
+        }
+
+        public static Func<Person, string> GetFormatter(string type)
+        {
+            if (type == "name")
+            {
+                return p => p.Name;
+            }
+            else if (type == "age")
+            {
+                return p => p.Age.ToString();
+            }
+            else if (type == "name age")
+            {
+                return p => $"{p.Name} - {p.Age}";
+            }
+            return null;
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
