189452b13a5f0a50c44714d5716b8fb8a1cd990a
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 16:50:24 +0200

    100/100

---
+++ 07. KnightsOfHonor/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split().ToArray();
+
+            PrintNames(names, n => Console.WriteLine(n));
+
+        }
+
+        public static void PrintNames(string[] names, Action<string> action)
+        {
+            foreach (var name in names)
+            {
+                action($"Sir {name}");
+            }
+        }
+    }
+}
