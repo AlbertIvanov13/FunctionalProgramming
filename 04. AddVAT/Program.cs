12237a0e3992b6b874bc8ba9b569d56c5b44cb76
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 15:04:42 +0200

    100/100

---
+++ 04. AddVAT/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04._AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+
+            var results = Prices(prices);
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+
+        public static double[] Prices(double[] prices)
+        {
+            List<double> numbers = new List<double>();
+            foreach (var item in prices)
+            {
+                Func<double, double> func = x => item * 1.20;
+                numbers.Add(func(item));
+            }
+
+            return numbers.ToArray();
+        }
+    }
+}
