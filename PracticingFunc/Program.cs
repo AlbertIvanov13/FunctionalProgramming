bb1687c5188d2fbb7f15348e7de864c4b495d01b
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 12:33:19 +0200

    Training

---
+++ PracticingFunc/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticingFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            Func<double, float, int> combineFunc = Add;
+
+            Console.WriteLine(combineFunc(x, y));
+        }
+        public static int Add(double x, float y)
+        {
+            return (int)(x + y);
+        }
+    }
+}
