c52f5471308997a92acda34d57b26a5baedd2798
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 14:47:28 +0200

    100/100

--- 03. CountUppercaseWords/Program.cs
+++ 03. CountUppercaseWords/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var result = UppercaseWord(words);
+
+            foreach ( var word in result )
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public static string[] UppercaseWord(string[] words)
+        {
+            List<string> uppercaseWords = new List<string>();
+
+
+            foreach (string word in words)
+            {
+                Func<string, bool> func = (word) => char.IsUpper(word[0]);
+                if (func(word))
+                {
+                    uppercaseWords.Add(word);
+                }
+            }
+
+            return uppercaseWords.ToArray();
         }
     }
 }
