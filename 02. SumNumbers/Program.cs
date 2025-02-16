471ddfc6018f38fab00f0d0ad23dcc5982365958
Albert Ivanov <albertivanov187@gmail.com>
2025-02-16 12:17:27 +0200

    100/100

---
+++ 02. SumNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace SumNumbers
+{
+    public class SumNumbers
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(NumbersCount(numbers));
+            Console.WriteLine(NumbersSum(numbers));
+
+        }
+        static int NumbersCount(int[] numbers)
+        {
+            int count = 0;
+            foreach (var item in numbers)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        static int NumbersSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
