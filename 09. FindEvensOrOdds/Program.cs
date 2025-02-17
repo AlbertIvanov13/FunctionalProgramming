bf6128ee0c5075473232ce234ae337ca390dd07b
Albert Ivanov <albertivanov187@gmail.com>
2025-02-17 16:55:07 +0200

    100/100

---
+++ 09. FindEvensOrOdds/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _09._FindEvensOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ranges = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int lowerRange = ranges[0];
+            int upperRange = ranges[1];
+            HashSet<int> numbers = new HashSet<int>();
+
+            string command = Console.ReadLine();
+
+            Predicate<int> check = n => n % 2 == 0;
+
+            for (int i = lowerRange; i <= upperRange; i++)
+            {
+                if (command == "even")
+                {
+                    if (check(i))
+                    {
+                        numbers.Add(i);
+                    }
+                }
+                else
+                {
+                    if (!check(i))
+                    {
+                        numbers.Add(i);
+                    }
+                }
+            }
+
+            Console.WriteLine($"{string.Join(" ", numbers)}");
+        }
+    }
+}
