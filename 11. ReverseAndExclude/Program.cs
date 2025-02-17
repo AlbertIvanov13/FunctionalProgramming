42c9bb77d063f518b67424f9e148449e4b7d65c9
Albert Ivanov <albertivanov187@gmail.com>
2025-02-17 22:18:38 +0200

    100/100

---
+++ 11. ReverseAndExclude/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _11._ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            Predicate<int> divideCheck = n => n % divisor == 0;
+
+            Func<int[], int[]> function = nums =>
+            {
+                List<int> nonDivisibleNumbers = new List<int>();
+                List<int> newNums = nums.ToList();
+                for (int i = (nums.Length) - (1); i >= 0; i--)
+                {
+                    if (divideCheck(nums[i]))
+                    {
+                        newNums.Remove(nums[i]);
+                    }
+                    else
+                    {
+                        nonDivisibleNumbers.Add(nums[i]);
+                    }
+                }
+
+                return nonDivisibleNumbers.ToArray();
+            };
+
+            Console.WriteLine(string.Join(" ", function(numbers)));
+        }
+    }
+}
