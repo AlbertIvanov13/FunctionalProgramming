866a3bd4e4ae3e6f04795d9c9d6ff87d3f0d791b
Albert Ivanov <albertivanov187@gmail.com>
2025-02-17 22:33:29 +0200

    100/100

---
+++ 12. PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _12._PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int namesLength = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split().ToArray();
+
+            List<string> checkedNames = new List<string>();
+
+            Predicate<string> checker = n => n.Length <= namesLength;
+
+            Func<string[], string[]> nameCheck = checkNames =>
+            {
+                foreach (var name in names)
+                {
+                    if (checker(name))
+                    {
+                        checkedNames.Add(name);
+                    }
+                }
+
+                return checkedNames.ToArray();
+            };
+
+            Console.WriteLine($"{string.Join(Environment.NewLine, nameCheck(names))}");
+        }
+    }
+}
