5b005da8b503f0685c3638778464c570206ca158
Albert Ivanov <albertivanov187@gmail.com>
2025-02-17 16:41:15 +0200

    100/100

---
+++ 08. CustomMinFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _08._CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numbers = new HashSet<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Func<int, int, bool> number = (number, lower) => number < lower;
+
+            int checkNumber = int.MaxValue;
+            foreach (var item in numbers)
+            {
+                if (number(item, checkNumber))
+                {
+                    checkNumber = item;
+                }
+            }
+
+            Console.WriteLine(checkNumber);
+        }
+    }
+}
