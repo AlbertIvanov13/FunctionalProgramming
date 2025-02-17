137658c5ae5b055f038a1d3a8af618d067d14804
Albert Ivanov <albertivanov187@gmail.com>
2025-02-17 17:33:46 +0200

    100/100

---
+++ 10. AppliedArithmetics/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _10._AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int[]> addingFunc = numbers =>
+            {
+                List<int> newList = new List<int>(numbers.Length);
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    newList.Add(numbers[i]++);
+                }
+                numbers = newList.ToArray();
+                return numbers;
+            };
+
+            Func<int[], int[]> multiplyFunc = numbers =>
+            {
+                List<int> newList = new List<int>(numbers.Length);
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    newList.Add(numbers[i]*=2);
+                }
+                numbers = newList.ToArray();
+                return numbers;
+            };
+
+            Func<int[], int[]> subtractFunc = numbers =>
+            {
+                List<int> newList = new List<int>(numbers.Length);
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    newList.Add(numbers[i]--);
+                }
+                numbers = newList.ToArray();
+                return numbers;
+            };
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        addingFunc(numbers); break;
+                    case "multiply":
+                        multiplyFunc(numbers); break;
+                    case "subtract":
+                        subtractFunc(numbers); break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", numbers));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
